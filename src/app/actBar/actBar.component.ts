8159297e0c3021de740af57f7a170f9764e92b9e
Jlopezr50 <jlopezr50@miumg.edu.gt>
2021-10-13 00:44:50 -0600

    Navegacion entre componentes CRUD

--- src/app/actBar/actBar.component.ts
+++ src/app/actBar/actBar.component.ts
@@ -1,14 +1,28 @@
 import { Component, OnInit } from '@angular/core'
+import {RouterExtensions} from "@nativescript/angular";
 
 
 @Component({
-    selector: 'actBar',
+  moduleId: module.id,
+  selector: 'actBar',
     templateUrl: './actBar.component.html',
 })
 export class ActComponent implements OnInit {
+  constructor(
+    private router: RouterExtensions
+  ) { }
+  ngOnInit() { }
 
-    constructor() { }
-
-    ngOnInit(): void {
-    }
+  public navigateCreate(){
+    this.router.navigate(['/create'])
+  }
+  public navigateDelete(){
+    this.router.navigate(['/delete'])
+  }
+  public navigateRead(){
+    this.router.navigate(['/read'])
+  }
+  public navigateUpdate(){
+    this.router.navigate(['/update'])
+  }
 }
